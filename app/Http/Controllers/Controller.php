7063d49eb90ea20ab0b18c00a1c067cdc45a905e
Duvan Florez <duvanfh11@gmail.com>
2026-08-29 19:23:06 -0500

    Se crea nueva funcionalidad dentro del controlador principal para manejar respuestas de la API y se corrigen nombres de archivos

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,7 +2,34 @@
 
 namespace App\Http\Controllers;
 
+use BcMath\Number;
+use Exception;
+use Illuminate\Database\QueryException;
+
 abstract class Controller
 {
-    //
+    protected function handleResponse(
+        bool $success ,
+        string $message , 
+        int $state, 
+        ?array $data = null,
+        ?string $exception = null,
+        ?string $error_code = null,
+    ){
+        if($exception && $error_code){
+            return response()->json([
+                'success' => $success,
+                'message' => $message,
+                'exception' => $exception,
+                'error_code' => $error_code                
+            ],$state);
+            
+        }else{
+            return response()->json([
+                'success' => $success,
+                'message' => $message,
+                'data' => $data
+            ],$state);
+        }
+    }
 }
