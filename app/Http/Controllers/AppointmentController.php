6f3cd09648b69f74bbaf15088c257131c30bd604
Duvan Florez <duvanfh11@gmail.com>
2026-09-06 23:10:38 -0500

    Se hace el desarrollo del método index para las vistas de cargos y roles

--- app/Http/Controllers/AppointmentController.php
+++ app/Http/Controllers/AppointmentController.php
@@ -4,15 +4,28 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\AppointmentRequest;
 use App\Models\Appointment;
+use App\Services\AppointmentService;
+use Exception;
+use Illuminate\Database\QueryException;
 
 class AppointmentController extends Controller
 {
+    public function __construct(
+        protected AppointmentService $service
+    ){}
     /**
      * Display a listing of the resource.
      */
     public function index()
     {
-        //
+        try{
+            $response = $this->service->getAll();
+            return $this->handleResponse(true, 'Se cargaron los cargos correctamente', 200, $response);
+        }catch(QueryException $e){
+            return $this->handleResponse(false, 'Error al conectar con la base de datos', 500, null, $e->getMessage(), "DATABASE_ERROR");
+        }catch(Exception $e){
+            return $this->handleResponse(false, 'Error inesperado del servidor', 500, null, $e->getMessage(), "SERVER_ERROR");
+        }
     }
 
     /**
--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Http\Controllers\AppointmentController;
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\ProductController;
+use App\Http\Controllers\RoleController;
 use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -21,11 +23,24 @@ Route::middleware('auth:sanctum')->group(function(){
     Route::get('/products', [ProductController::class, 'index']);
     Route::post('/products', [ProductController::class, 'store']);
     Route::get('/products/{product}', [ProductController::class, 'show']);
+    
+    //ROLES
+    Route::get('/roles', [RoleController::class, 'index']);
+    Route::post('/roles', [RoleController::class, 'store']);
+    Route::post('/roles/{role}', [RoleController::class, 'show']);
+
+    // APPOINTMENTS
+    Route::get('/appointments', [AppointmentController::class, 'index']);
+    Route::post('/appointmets', [AppointmentController::class, 'store']);
+    Route::get('/appointments/{appointment}', [AppointmentController::class, 'show']);
 
     //CATEGORIES
     Route::get('/categories', [CategoryController::class, 'index']);
     Route::post('/categories', [CategoryController::class, 'store']);
     Route::get('/categories/{category}', [CategoryController::class, 'show']);
+
+    //APPOINTMENTS
+
 });
 
 Route::post('/register', [UserController::class, 'store']);
