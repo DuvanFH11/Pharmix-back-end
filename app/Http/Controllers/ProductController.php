214eb090d1ef53154e0082c31565835801db0466
Duvan Florez <duvanfh11@gmail.com>
2026-09-23 18:52:41 -0500

    Se realiza desarrollo de funcionalidad show y storeOrUpdate para productos

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -47,9 +47,16 @@ class ProductController extends Controller
     /**
      * Display the specified resource.
      */
-    public function show(Product $product)
+    public function show(Int $productId)
     {
-        //
+        try{
+            $response = $this->service->show($productId);
+            return $this->handleResponse(true, 'Usuario cargado correctamente', 200, $response);
+        }catch(QueryException $e){
+            return $this->handleResponse(false, 'Lo sentimos, algo salió mal al procesar la solicitud, vuelve a intentarlo.', 500, null, $e->getMessage(), "DATABASE_ERROR");
+        }catch(Exception $e){
+            return $this->handleResponse(false, 'El sistema no está disponible temporalmente, intentalo más tarde.', 500, null, $e->getMessage(), "SERVER_ERROR");
+        }
     }
 
     /**
--- app/Services/ProductService.php
+++ app/Services/ProductService.php
@@ -2,6 +2,9 @@
 namespace App\Services;
 
 use App\Models\Product;
+use Exception;
+use Illuminate\Database\QueryException;
+use Illuminate\Support\Facades\DB;
 
 Class ProductService{
     public function __construct(
@@ -11,5 +14,33 @@ Class ProductService{
     public function getAll(){
         return $this->model->select(['id', 'name', 'brand','description', 'unit_price', 'package_price', 'invima_registration', 'strength', 'unit', 'user_creator'])
         ->with(['user_creator:id, name'])->get()->toArray();
+    }
+
+    public function show($productId){
+        return $this->model->select(['id', 'name', 'brand','description', 'unit_price', 'package_price', 'invima_registration', 'strength', 'unit', 'user_creator'])
+        ->find($productId)->toArray();
+    }
+
+    public function storeOrUpdate($data, $id){
+        try{ 
+            $productData = [
+                'name' => $data['name'],
+                'brand' => $data['brand'],
+                'description' => $data['description'],
+                'package_price' => $data['package_price'],
+                'unit_price' => $data['unit_price'],
+                'invima_registration' => $data['invima_registration'],
+                'strength' => $data['strength'],
+                'unit' => $data['unit'],
+                'is_active' => true
+            ];
+            DB::beginTransaction(); 
+                Product::updateOrcreate(['id' => $id], $productData);
+            DB::commit();
+        }catch(QueryException $e){
+            DB::rollBack();
+        }catch(Exception $e){
+            DB::rollBack();
+        }
     }
 }
